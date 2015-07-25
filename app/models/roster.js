5bb031aad597b3cdc09d3f4547cf053479b55cb1
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-07-24 21:39:09 -0400

    Added the component completely with model object being rendered in the component template

--- app/models/roster.js
+++ app/models/roster.js
@@ -2,33 +2,40 @@ import DS from 'ember-data';
 
 var Roster = DS.Model.extend({
   name: DS.attr('string'),
+  email: DS.attr('string')
 });
 
 Roster.reopenClass({
 	FIXTURES: [
 		{
 			id: 1,
-			name: "Daniel Dvorkin"
+			name: "Daniel Dvorkin",
+			email: "Dvorkin212@me.com"
 		},
 		{
 			id:2,
-			name: "Amanda Karta"
+			name: "Amanda Karta",
+			email: "Dvorkin212@me.com"
 		},
 		{
 			id: 3,
-			name: "Daniel Dvorkin"
+			name: "Daniel Dvorkin",
+			email: "Dvorkin212@me.com"
 		},
 		{
 			id:4,
-			name: "Amanda Karta"
+			name: "Amanda Karta",
+			email: "Dvorkin212@me.com"
 		},
 		{
 			id: 5,
-			name: "Daniel Dvorkin"
+			name: "Daniel Dvorkin",
+			email: "Dvorkin212@me.com"
 		},
 		{
 			id:6,
-			name: "Amanda Karta"
+			name: "Amanda Karta",
+			email: "Dvorkin212@me.com"
 		},
 	]
 });
