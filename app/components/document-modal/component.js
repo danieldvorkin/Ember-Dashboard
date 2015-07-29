d66d3385b480cff365d587b7624368947bf3acce
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-07-29 15:52:44 -0400

    Added a validation on file size with an alert message notifying the user that the file is too big

--- app/components/document-modal/component.js
+++ app/components/document-modal/component.js
@@ -5,13 +5,18 @@ export default Ember.Component.extend({
 		fileLoaded: function(file){
 			console.log(file.filename, file.type, file.size, file.data);
 			var newSize = file.size / 1024;
-			var doc = {
-				fileName: file.filename,
-				fileType: file.type,
-				fileSize: newSize,
-				link: file.data
-			};
-			this.sendAction('compAction', doc);
+			if(file.size > 70){
+				alert("File is too big");
+			}
+			else {
+				var doc = {
+					fileName: file.filename,
+					fileType: file.type,
+					fileSize: newSize,
+					link: file.data
+				};
+				this.sendAction('compAction', doc);
+			}
 		}
 	}
 });
