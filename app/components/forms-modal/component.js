ff920f2df4efabb9925c6f627e5dcc2604064ff9
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-07-30 15:55:40 -0400

    Removed size check for forms, wasn't working out and its not needed

--- app/components/forms-modal/component.js
+++ app/components/forms-modal/component.js
@@ -3,16 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	actions: {
 		fileLoaded: function(file){
-			if(file.size > 85){
-				alert('File is too big.... sorry, try again!!!!!');
-			}else {
-				var form = {
-					title: file.filename,
-					details: this.get('details'),
-					link: file.data
-				};
-				this.sendAction('compAction', form);
-			}
+			var form = {
+				title: file.filename,
+				details: this.get('details'),
+				link: file.data
+			};
+			this.sendAction('compAction', form);
 		}
 	}
 });
