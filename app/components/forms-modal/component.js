9e96142efc7211d3bc8bbde2e1693877303da54c
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-07-30 15:53:29 -0400

    Added a check for form upload to be less than 85 kb in size, if bigger, alert user and request for retry

--- app/components/forms-modal/component.js
+++ app/components/forms-modal/component.js
@@ -3,13 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	actions: {
 		fileLoaded: function(file){
-			console.log(file.filename, this.get('details'));
-			var form = {
-				title: file.filename,
-				details: this.get('details'),
-				link: file.data
-			};
-			this.sendAction('compAction', form);
+			if(file.size > 85){
+				alert('File is too big.... sorry, try again!!!!!');
+			}else {
+				var form = {
+					title: file.filename,
+					details: this.get('details'),
+					link: file.data
+				};
+				this.sendAction('compAction', form);
+			}
 		}
 	}
 });
