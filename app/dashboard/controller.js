a0c0a596d575cf5bd53d49053a273431ae5e281f
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-08-06 13:16:17 -0400

    Get the icons rendering next to eachother rather than on top of eachother

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,5 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	
+	icons: (function() {
+		return [
+			{
+				link: 'https://portal.office.com/Home',
+				img: 'assets/images/icons_email.png'
+			},
+			{
+				link: '',
+				img: ''
+			},
+			{
+				link: '',
+				img: ''
+			},
+			{
+				link: '',
+				img: ''
+			},
+			{
+				link: '',
+				img: ''
+			},
+			{
+				link: '',
+				img: ''
+			},
+			{
+				link: '',
+				img: ''
+			},
+		];
+	}).property()
 });
