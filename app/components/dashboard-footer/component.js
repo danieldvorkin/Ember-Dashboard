afce64a54ddcd49cdb51b3653473b5a60e869111
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-08-07 19:03:50 -0400

    Attempted to add tooltips so users can know what the application is, rather than reading the url

--- app/components/dashboard-footer/component.js
+++ app/components/dashboard-footer/component.js
@@ -17,56 +17,67 @@ export default Ember.Component.extend({
 	icons: (function() {
 		return [
 			{
+				name: 'Office Portal',
 				link: 'https://portal.office.com/Home',
 				img: 'assets/images/icons_email.png',
 				notifications: 1
 			},
 			{
+				name: 'Chat',
 				link: '#',
 				img: 'assets/images/icons_chat.png',
 				notifications: 2
 			},
 			{
+				name: 'Phone',
 				link: '#',
 				img: 'assets/images/icons_phone.png',
 				notifications: 3
 			},
 			{
+				name: 'EMR',
 				link: '#',
 				img: 'assets/images/icons_emr.png',
 				notifications: 4
 			},
 			{
+				name: 'IAR - William Oslerhs',
 				link: 'https://iar.williamoslerhs.ca/concerto/Login.htm',
 				img: 'assets/images/icons_iar.png',
 				notifications: 0
 			},
 			{
+				name: "Roxy Software",
 				link: 'https://parc.roxysoftware.com/login.asp',
 				img: 'assets/images/icons_roxy.png',
 				notifications: 0
 			},
 			{
+				name: "vConsult.me",
 				link: 'https://demo.vconsult.me',
 				img: 'assets/images/icons_vC.png',
 				notifications: 0
 			},
 			{
+				name: "Portal Office",
 				link: 'https://portal.office.com/Home',
 				img: 'assets/images/icons_office.png',
 				notifications: 0
 			},
 			{
+				name: "Obsidix",
 				link: 'https://poc.obsidix.com/',
 				img: 'assets/images/icons_obsidix.png',
 				notifications: 0
 			},
 			{
+				name: "RDWeb",
 				link: 'https://poc.obsidix.com/RDWeb/',
 				img: 'assets/images/icons_mydesctop.png',
 				notifications: 0
 			},
 			{
+				name: "Icon Settings",
 				link: 'https://portal.office.com/Home',
 				img: 'assets/images/icons_setting.png',
 				notifications: 0
